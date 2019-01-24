725c536e7fb76f8aeb4dea6d5f56622cb873da39
wangjiesong <wjs4d@163.com>
2019-01-24 10:26:48 +0800

    Update curl_test.c

--- curl_test.c
+++ curl_test.c
@@ -10,13 +10,26 @@ int main(int argc, char **argv)
 {
     int rt=0;
     char rcv_buf[8192];
+    char url="http://10.12.2.8:7902/open/check.ashx";
+    char send_data="datajson=\
+    {\
+        \"action\":\"checkid\",\
+        \"params\":{\
+            \"localonly\":1,\
+            \"btype\":\"01\",\
+            \"channelid\":\"IC\",\
+            \"idname\":\"?M㈡~V规~X~N\",\
+            \"idno\":\"320681198706280211\"\,\
+            \"accesskey\":\"957971E9222B1D46D8059252711E0E50\"\
+        }\
+    }";
     /*1 全局初始化 进程启动时调用 多线程环境下只能调用一次*/
     rt = curl_global_init(CURL_GLOBAL_ALL);
     /*2 easy句柄初始化*/
     CURL *easy_handle = curl_easy_init();
     /*3 设置传输选项,传入URL,json数据报等*/
-    curl_easy_setopt(easy_handle, CURLOPT_URL, "http://10.12.2.8:7902/open/check.ashx" );
-    curl_easy_setopt(easy_handle, CURLOPT_POSTFIELDS,"datajson={\"action\":\"checkid\",\"params\":{\"localonly\":1,\"btype\":\"01\",\"channelid\":\"IC\",\"idname\":\"?M㈡~V规~X~N\",\"idno\":\"320681198706280211\",\"accesskey\":\"957971E9222B1D46D8059252711E0E50\"}}" );
+    curl_easy_setopt(easy_handle, CURLOPT_URL, url );
+    curl_easy_setopt(easy_handle, CURLOPT_POSTFIELDS,send_data );
     /*4 设置的传输选项，实现回调函数以完成用户特定任务*/
     curl_easy_setopt(easy_handle,CURLOPT_WRITEFUNCTION,write_data);
     curl_easy_setopt(easy_handle, CURLOPT_WRITEDATA, rcv_buf );
